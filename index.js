0a4085daa2c354be16c969fccbc34e90813c01a2
Thomas Lever <thomaslever508@gmail.com>
2018-06-13 14:01:24 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,13 @@
+function iterativeLog(array){
+  array.forEach(console.log(`${index}: ${element}`))
+}
+
+function iterate(callback){
+  var myArray = ['NYC', 'SF', 'LA', 'DC']
+  myArray.forEach(callback)
+  return myArray
+}
+
+function doToArray(array,callback){
+  array.forEach(callback)
+}
